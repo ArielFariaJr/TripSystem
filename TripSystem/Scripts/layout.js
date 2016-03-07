95a4eb558b4944b5273983581e695e1080fcd75c
Moyzes Braz <moyzes@gmail.com>
2016-03-07 00:11:45 -0300

    Layout changes

--- TripSystem/Scripts/layout.js
+++ TripSystem/Scripts/layout.js
@@ -2,14 +2,36 @@
 
 $(document).ready(function () {
     
+    adjustLogo();
     hideIntranetMenu();
 
 });
 
+function adjustLogo() {
+   
+    var href = document.location.href;
+
+    if (Utils.isHome(href)) {
+        $("#logo").removeClass("logo_content");
+        $("#logo").addClass("logo_home");
+    } else {
+        $("#logo").removeClass("logo_home");
+        $("#logo").addClass("logo_content");
+    }
+
+}
+
+/**
+* Method which strips out the pre-built menu and 
+* re-build it in a separate intrant menu.
+*/
 function hideIntranetMenu() {
 
+    //Small fix to remove empty <li> elements from menu.
+    $("#menu").find("li").get(3).remove();
+
     //Set up the arrays: one for the menus to be selected and initialize the <LI> collection.
-    var menuItems = ["/Genero", "/Guia", "/Transportadora", "/Veiculo", "/Excurcao"];
+    var menuItems = ["/Genero", "/Guia", "/Transportadora", "/Veiculo", "/Excurcao", "/Home/ListaEmbarque"];
     var liItems = [];
 
     //Loop thru the menus items do hide and collect to the <LI> colleciton.
@@ -20,7 +42,6 @@ function hideIntranetMenu() {
         }
     });
 
-    console.log(liItems.length);
     if (liItems.length > 0) {
 
         //Create a new <LI> to host the new intranet menu.
@@ -50,5 +71,17 @@ function hideIntranetMenu() {
                 $(intranetUl).fadeOut(100);
             }, 200);
         });
+    }
+}
+
+/** UTILS **/
+var Utils = {
+    isHome: function (href) {
+        var slashes = (href.match(/\//g) || []).length;
+        if (slashes == 3 && href.indexOf("?") == -1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
---
+++ TripSystem/Scripts/store.js
@@ -0,0 +1,73 @@
+﻿$(document).ready(function () {
+
+    //build the trip gallery
+    tripGallery();
+
+    //fire the first link
+    $($(".category-list").find("a").get(0)).click();
+
+
+});
+
+function tripGallery() {
+
+    //Loop for every lin in the category list.
+    $(".category-list").find("a").click(function(e) {
+
+        //prevent link default function
+        e.preventDefault();
+
+        //hightlight the item selection
+        $(".category-list").find("li").css("backgroundColor", "#807EA3");
+        $(this).parent().css("backgroundColor", "#696892");
+
+        //get the link to the detail
+        var href = $(this).attr("href");
+
+        $.get(href, function (data) {
+
+            //clear the trip list container
+            $("#list-trips").empty();
+
+            //get the framework-generated galery list
+            var ul = $(data).find("#album-list");
+            
+            //loop thru every trip
+            $(ul).find("li").each(function (index) {
+
+                //Build a detail object
+                var detail = new Detail($($(this).find("span").get(0)).text(),
+                    $(this).find("img").get(0),
+                    $($(this).find("a").get(0)).attr("href"));
+                console.log(detail);
+
+                //create a div for the trip and append it to the container
+                var div = $("<div class='trip' />");
+                 $(div).css("backgroundImage", "url('" + detail.img.src + "')");
+                var linkDetail = $("<a class='trip-title' />");
+                $(linkDetail).attr("href", detail.link);
+                $(linkDetail).text(detail.title);
+                $(div).append(linkDetail);
+                $("#list-trips").append(div);
+
+            });
+
+            //get the container height and hack the height of the whole page
+            var tripListHeight = $("#list-trips").height();
+            console.log(tripListHeight);
+            $("#layout-fix").css("height", (tripListHeight + 40) + "px");
+
+        });
+    });
+}
+
+/**
+* Trip detail class
+*/
+var Detail = function(title, img, link) {
+    
+    this.title = title;
+    this.img = img;
+    this.link = link;
+
+}
